a69e9ab2e48acffc583bb30140177f461e3f94eb
ash <339605@colum.edu>
2020-04-24 07:58:52 -0500

    final steps
    Final steps

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
@@ -15,8 +17,8 @@ public class GameEnding : MonoBehaviour
     bool m_IsPlayerCaught;
     float m_Timer;
     bool m_HasAudioPlayed;
-    
-    void OnTriggerEnter (Collider other)
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
@@ -24,44 +26,50 @@ public class GameEnding : MonoBehaviour
         }
     }
 
-    public void CaughtPlayer ()
+    public void CaughtPlayer()
     {
         m_IsPlayerCaught = true;
     }
 
-    void Update ()
+    void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
         }
+
+
         else if (m_IsPlayerCaught)
         {
-            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+        }
+
+        void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+        {
+            if (!m_HasAudioPlayed)
+            {
+                audioSource.Play();
+                m_HasAudioPlayed = true;
+            }
+
+            m_Timer += Time.deltaTime;
+            imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+            if (m_Timer > fadeDuration + displayImageDuration)
+            {
+                if (doRestart)
+                {
+                    SceneManager.LoadScene(0);
+                }
+                else
+                {
+                    Application.Quit();
+                }
+            }
         }
     }
 
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
-    {
-        if (!m_HasAudioPlayed)
-        {
-            audioSource.Play();
-            m_HasAudioPlayed = true;
-        }
-            
-        m_Timer += Time.deltaTime;
-        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        if (m_Timer > fadeDuration + displayImageDuration)
-        {
-            if (doRestart)
-            {
-                SceneManager.LoadScene (0);
-            }
-            else
-            {
-                Application.Quit ();
-            }
-        }
-    }
-}
+
+
